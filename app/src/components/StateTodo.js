d31fae414d4e2b442479c9bfa4acf2f8edab01ce
kaito <kaito1013kaito@gmail.com>
2024-03-22 22:15:06 +0900

    todo 完了削除ソートの機能を追加

--- app/src/components/StateTodo.js
+++ app/src/components/StateTodo.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import './StateTodo.css';
 
 let maxId=0;
 
 export default function StateTodo() {
     const [title, setTitle] = useState('');
     const [todos, setTodo] = useState([]);
+    const [desc, setDesc] = useState(true);
 
     const handleChangeTodo = e => {
         setTitle(e.target.value);
@@ -24,6 +26,38 @@ export default function StateTodo() {
         setTitle('');
     };
 
+    const handleDone = e => {
+        setTodo(todos.map(todo => {
+            if (todo.id === Number(e.target.dataset.id)) {
+                return {
+                    ...todo,
+                    done: true
+                };
+            }else{
+                return todo;
+            }
+        }))
+    };
+
+    const handleRemove = e => {
+        setTodo(todos.filter(todo =>
+            todo.id !==Number(e.target.dataset.id)
+        ));
+    };
+
+    const handleSort = () => {
+        const sorted = [...todos];
+        sorted.sort((a,b)=>{
+            if(desc){
+                return a.created.getTime() - b.created.getTime();
+            }else{
+                return b.created.getTime() - a.created.getTime();
+            }
+        })
+        setDesc(!desc);
+        setTodo(sorted);
+    };
+
     return (
         <div>
             <label>
@@ -31,16 +65,28 @@ export default function StateTodo() {
                 <input type="text" value={title} onChange={handleChangeTodo} />
             </label>
             <button type="button" onClick={handleClick}>追加</button>
-
+            <button type="button" onClick={handleSort}>並び替え({desc ? '↑' : '↓'})</button>
             <hr />
 
             <ul>
                 {todos.map(todo => (
-                    <li key={todo.id}>
+                    <li key={todo.id} className={todo.done ? 'done' : ''}>
                         {todo.title}
+                        <button 
+                            type="button" 
+                            onClick={handleDone}
+                            data-id={todo.id} // ここでidを設定することでe.target.dataset.idで取得できる
+                        >
+                                済</button>
+                                <button 
+                            type="button" 
+                            onClick={handleRemove}
+                            data-id={todo.id} // ここでidを設定することでe.target.dataset.idで取得できる
+                        >
+                                削除</button>
                     </li>
                 ))}
             </ul>
         </div>
-    )
+    );
 };

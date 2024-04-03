1f74575eaeec443055cbb5ade05ebb1ed3ebee0d
kaito <kaito1013kaito@gmail.com>
2024-04-04 01:44:42 +0900

    light or dark mode

--- app/src/index.js
+++ app/src/index.js
@@ -28,13 +28,14 @@ import MaterialGrid from './components/MaterialGrid';
 
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import theme from './components/theme';
+import MaterialMode from './components/MaterialMode';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <MaterialBasic />
+      <MaterialMode />
     </ThemeProvider>
   </React.StrictMode>
 );

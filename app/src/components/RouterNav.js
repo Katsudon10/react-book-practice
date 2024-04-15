cc86099b7070fa23a22518ceb43f5c7bdb763b1a
kaito <kaito1013kaito@gmail.com>
2024-04-15 11:48:53 +0900

    NavLink

---
+++ app/src/components/RouterNav.js
@@ -0,0 +1,21 @@
+import { NavLink,Outlet } from "react-router-dom";
+import './RouterNav.css';
+
+const isCurrent = ({isActive, isPending}) => isActive ? {
+    color: 'Red',
+    fontWeight: 'bold'
+} : {};
+
+export default function RouterNav() {
+    return (
+        <>
+            <ul>
+                <li><NavLink style={isCurrent} to="/">Home</NavLink></li>
+                <li><NavLink style={isCurrent} to="/article">Article</NavLink></li>
+                <li><NavLink style={isCurrent} to="/about">About</NavLink></li>
+            </ul>
+            <hr />
+            <Outlet />
+        </>
+    );
+}
--- app/src/components/routesLink.js
+++ app/src/components/routesLink.js
@@ -3,16 +3,15 @@ import RouterApp from "./RouterApp";
 import TopPage from "./TopPage";
 import ArticlePage from "./ArticlePage";
 import AboutPage from "./AboutPage";
+import RouterNav from "./RouterNav";
 
 const RouteLink = createBrowserRouter(
     createRoutesFromElements(
-        <>
-            <Route element={<RouterApp />} >
-                <Route index element={<TopPage />} />
-                <Route path="article" element={<ArticlePage />} />
-                <Route path="about" element={<AboutPage />} />
-            </Route>
-        </>
+        <Route element={<RouterNav />} >
+            <Route index element={<TopPage />} />
+            <Route path="article" element={<ArticlePage />} />
+            <Route path="about" element={<AboutPage />} />
+        </Route>
     )
 );
 

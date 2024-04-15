06b58138f67a046be2b848fe31a2e0e24946d90c
kaito <kaito1013kaito@gmail.com>
2024-04-15 10:48:25 +0900

    routeをオブジェクトからタグに変更

--- app/src/components/routesBasic.js
+++ app/src/components/routesBasic.js
@@ -1,13 +1,17 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 
 import TopPage from './TopPage';
 import ArticlePage from './ArticlePage';
 import AboutPage from './AboutPage';
 
-const routesBasic = createBrowserRouter([
-  { path: '/', element: <TopPage /> },
-  { path: '/article', element: <ArticlePage /> },
-  { path: '/about', element: <AboutPage /> },
-], { basename: '/myapp' });
+const routesBasic = createBrowserRouter(
+    createRoutesFromElements(
+        <>
+            <Route path="/" element={<TopPage />} />
+            <Route path="/article" element={<ArticlePage />} />
+            <Route path="/about" element={<AboutPage />} />
+        </>
+    )
+);
 
 export default routesBasic;

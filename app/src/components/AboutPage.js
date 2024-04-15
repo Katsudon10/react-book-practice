0ea2ef0fd0f68719d2e999757454d3120e247623
kaito <kaito1013kaito@gmail.com>
2024-04-15 12:06:30 +0900

    ボタンを押したときにページ移動

--- app/src/components/AboutPage.js
+++ app/src/components/AboutPage.js
@@ -1,7 +1,13 @@
+import { useNavigate } from "react-router-dom";
+
 export default function AboutPage() {
+  const navigate = useNavigate();
+
+  const handleClick = () => navigate("/");
   return (
     <>
       <p>Aboutページです。</p>
+      <button type="button" onClick={handleClick}>Top</button>
     </>
   );
 }
